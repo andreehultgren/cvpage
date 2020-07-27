6fccef5f3a4eceb44b35d0c2507e55e310544aff
Andree Hultgren <andree_hultgren@hotmail.com>
2020-07-27 21:06:53 +0200

    Updates

---
+++ precache-manifest.77f19f250debd14a0ae8462b6fc738f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049dd139589ab794115d8785cd6368a2",
+    "url": "/cvpage/index.html"
+  },
+  {
+    "revision": "a0acb75c1bb0cacb5e1d",
+    "url": "/cvpage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d23b28d8d35c27b7f6e",
+    "url": "/cvpage/static/js/2.c6aa0c64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cvpage/static/js/2.c6aa0c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0acb75c1bb0cacb5e1d",
+    "url": "/cvpage/static/js/main.5b7d12b6.chunk.js"
+  },
+  {
+    "revision": "720c8a2d16a32a1fbf02",
+    "url": "/cvpage/static/js/runtime-main.01ad95a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cvpage/static/media/logo.5d5d9eef.svg"
+  }
+]);
