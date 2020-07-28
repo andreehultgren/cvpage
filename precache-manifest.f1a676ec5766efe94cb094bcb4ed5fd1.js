1714989b0cfb7b292e6e4061fb063275d5c346fc
Andree Hultgren <andree_hultgren@hotmail.com>
2020-07-28 11:27:35 +0200

    Updates

---
+++ precache-manifest.f1a676ec5766efe94cb094bcb4ed5fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dc1152c7ff5e5a69454bcac4f8c4c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc37ce35bd03543b7aee",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "831fb7ad514676ded628",
+    "url": "/static/js/2.cbcdc88b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cbcdc88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc37ce35bd03543b7aee",
+    "url": "/static/js/main.8d9687a6.chunk.js"
+  },
+  {
+    "revision": "656de639d4338275ec06",
+    "url": "/static/js/runtime-main.d63555a6.js"
+  }
+]);
