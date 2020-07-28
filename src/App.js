4ee0d505b082652f0ab89b8e6f096d4cb8699247
Andree Hultgren <andree_hultgren@hotmail.com>
2020-07-28 11:17:47 +0200

    Add basic CV content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from "./components/Container"
+import ProfilePage from "./components/ProfilePage"
+import EducationPage from "./components/EducationPage"
+import ExperiencesPage from "./components/ExperiencesPage"
+import SkillsPage from "./components/SkillsPage"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <ProfilePage themeColor={"#D3FFFF"} title={"Profile"}/>
+        <EducationPage themeColor={"#DAFFF1"} title={"Education"}/>
+        <ExperiencesPage themeColor={"#FFE7EC"} title={"Experiences"}/>
+        <SkillsPage themeColor={"#FFF9F0"} title={"Skills"}/>        
+      </Container>
     </div>
   );
 }
---
+++ src/components/SkillsPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import ProfileSection from "./ProfileSection"
+
+import styles from './styles'
+import strings from '../strings'
+
+function SkillsPage(props) {
+    const { classes } = props;
+    return (
+        <div style={{height:"100%", width:"100%", backgroundColor:props.themeColor}} className={classes.wrap}>
+            <div className={classes.content}>
+                <div className={classes.pageContent}>
+                    {
+                    strings.SkillsPage.map(child=>
+                            <>
+                                <h1>{child.title}</h1>
+                                <p>{child.content}</p>
+                            </>
+                    )}
+                </div>
+            </div>
+            <ProfileSection/>            
+        </div>
+    )}
+
+export default withStyles(styles)(SkillsPage)
