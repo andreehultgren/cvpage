88501f8f4e05a15d5544f0d04da3fa31ef6838f9
Andree Hultgren <andree_hultgren@hotmail.com>
2020-08-01 18:51:12 +0200

    Updates

---
+++ precache-manifest.3cf4fd01c37545e2e1bdcf8ea20e34ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716899cc263cfb1974d10e98df1e41c7",
+    "url": "/cvpage/index.html"
+  },
+  {
+    "revision": "443f87cd9269d5434bc1",
+    "url": "/cvpage/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7f6fe124000f433cd146",
+    "url": "/cvpage/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "443f87cd9269d5434bc1",
+    "url": "/cvpage/static/js/2.84565e0d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/cvpage/static/js/2.84565e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6fe124000f433cd146",
+    "url": "/cvpage/static/js/main.02052443.chunk.js"
+  },
+  {
+    "revision": "2fedd0d140634adeefd5",
+    "url": "/cvpage/static/js/runtime-main.94cb04cd.js"
+  }
+]);
