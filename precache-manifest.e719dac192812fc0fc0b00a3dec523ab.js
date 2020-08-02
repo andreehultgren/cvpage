4ca4dbc5b0eee2e1287577ad08bf6dec36253c94
Andree Hultgren <andree_hultgren@hotmail.com>
2020-08-02 13:08:11 +0200

    Updates

--- precache-manifest.e719dac192812fc0fc0b00a3dec523ab.js
+++ precache-manifest.e719dac192812fc0fc0b00a3dec523ab.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ce6be4c1d0c41e2b2e97f94f7bf9d53",
+    "revision": "a78f28d9f566efb89663914259cf4b66",
     "url": "/cvpage/index.html"
   },
   {
-    "revision": "e8c50f92518884e709f3",
+    "revision": "a87dcea3a7268b13fc71",
     "url": "/cvpage/static/css/2.e53f431d.chunk.css"
   },
   {
-    "revision": "b4437f9924610f06e4c3",
+    "revision": "4f035aa7cd648ee81dab",
     "url": "/cvpage/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "e8c50f92518884e709f3",
-    "url": "/cvpage/static/js/2.9495f595.chunk.js"
+    "revision": "a87dcea3a7268b13fc71",
+    "url": "/cvpage/static/js/2.7e3239c3.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/cvpage/static/js/2.9495f595.chunk.js.LICENSE.txt"
+    "url": "/cvpage/static/js/2.7e3239c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4437f9924610f06e4c3",
-    "url": "/cvpage/static/js/main.224250dc.chunk.js"
+    "revision": "4f035aa7cd648ee81dab",
+    "url": "/cvpage/static/js/main.a348dff2.chunk.js"
   },
   {
     "revision": "2fedd0d140634adeefd5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cvpage/precache-manifest.42561b2b1cec009559a269f0edd59181.js"
+  "/cvpage/precache-manifest.e719dac192812fc0fc0b00a3dec523ab.js"
 );
 
 self.addEventListener('message', (event) => {
